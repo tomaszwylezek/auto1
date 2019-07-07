24e8e922fd7e2f7710a50244ed9e9a43e27269b0
Tomasz Wylężek <tomwylezek@gmail.com>
2019-07-07 12:17:37 +0200

    Add test to merchant deatails

--- src/App/Merchant/MerchantDetails/MerchantDetails.spec.tsx
+++ src/App/Merchant/MerchantDetails/MerchantDetails.spec.tsx
@@ -5,18 +5,29 @@ import { MerchantDetails } from './MerchantDetails';
 
 // Models
 import { IMerchantDetailsProps, IMerchantDetailsState } from './MerchantDetails.types';
+import { ButtonTheme } from 'model';
 
 describe('MerchantDetails component should', (): void => {
   let props: IMerchantDetailsProps;
   let component: ShallowWrapper<IMerchantDetailsProps, IMerchantDetailsState>;
 
   beforeEach((): void => {
-    props = {};
+    props = {
+      id: '123',
+      name: 'Foo Bar',
+      phone: '123',
+      onDeleteClick: jest.fn()
+    };
 
     component = shallow(<MerchantDetails {...props} />);
   });
 
   it('render correctly', (): void => {
     expect(component).toMatchSnapshot();
+  });
+
+  it('react on delete click', (): void => {
+    component.find({ theme: ButtonTheme.danger }).simulate('click');
+    expect(props.onDeleteClick).toHaveBeenCalled();
   });
 });
